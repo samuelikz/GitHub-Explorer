f4e4c7e7bf629bb300cbb9e786bb3393b23a267a
samuelikz <samuel.nds@outlook.com>
2021-11-30 17:03:56 -0300

    Fundamentos UseEfect

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,10 +1,11 @@
 export function RepositoryItem(props) {
     return (
         <li>
-            <strong>{props.repository?.name ?? 'Default'}</strong>
-            <p>{props.repository?.description ?? 'Default'}</p>
+            <strong>{props.repository.name}</strong>
+            <strong id="share">{props.repository.visibility}</strong>
+            <p>{props.repository.description}</p>
 
-            <a href={props.repository?.link ?? '#'}>
+            <a href={props.repository.html_url}>
                 Acessar Repositório
             </a>
         </li>
